8df456c948b9d9c533e900e89d0c9a819fed49d0
Đinh Ngọc Liên Nhi <dinhngocliennhi1999@gmail.com>
2023-02-25 12:18:21 +0700

    Update ProductFilterStatus.js

--- src/features/product/ProductFilterStatus.js
+++ src/features/product/ProductFilterStatus.js
@@ -1,41 +1,26 @@
 // material
-import FilterAltOutlinedIcon from '@mui/icons-material/FilterAltOutlined';
 import {
-  Box,
-  Button,
   Divider,
-  InputAdornment,
-
   Stack,
   Typography,
 } from "@mui/material";
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 
-import { useDispatch, useSelector } from "react-redux";
-import { FormProvider, FSelect } from "../../components/form";
-import Iconify from "../../components/Iconify/Iconify";
-import { handleClearFilters } from "./productSlice";
+import { useSelector } from "react-redux";
+import { FMultiCheckbox, FormProvider } from "../../components/form";import { handleClearFilters } from "./productSlice";
 
-
-
-export const FILTER_STATUS_OPTIONS = [
-  { value: "", label: "All" },
-  { value: "Top", label: "Top" },
-  { value: "New", label: "Newest" },
-  { value: "Discount", label: " Discount" },
-];
+export const FILTER_STATUS_OPTIONS = ["New", "Top", "Discount"];
 
 const defaultValues = {
-  sortBy: "",
-};
+  status: "",
 
+};
 
 export default function ProductFilterStatus({ handleDispatch }) {
   const { filters } = useSelector((state) => state?.product);
   const methods = useForm({ defaultValues, mode: "onChange" });
   const { handleSubmit, reset, watch } = methods;
-  const dispatch = useDispatch();
 
   useEffect(() => {
     if (!filters.sortBy) {
@@ -51,56 +36,26 @@ export default function ProductFilterStatus({ handleDispatch }) {
 
   return (
     <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
-      <Stack sx={{ p: 3, width: 200 }}>
-        <Stack spacing={1} sx={{mb:6}}>
+      <Stack sx={{ p: 2, width: 250 }}>
+        <Stack spacing={1} sx={{mb:1}}>
           <Typography
             sx={{
               color:"primary.main",
               fontSize: { xs: 15, md: 17, lg: 18 },
-              
-              textAlign: "center",
+              // textAlign: "center",
             }}
           >
             Product
           </Typography>
-          <FSelect
-            name="sortBy"
-            size="small"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <FilterAltOutlinedIcon />
-                </InputAdornment>
-              ),
-            }}
-          >
-            {FILTER_STATUS_OPTIONS.map((option) => (
-              <option key={option.value} value={option.value} sx={{ p: 4 }}>
-                {option.label}
-              </option>
-            ))}
-          </FSelect>
+           <FMultiCheckbox
+          name="status"
+          options={FILTER_STATUS_OPTIONS}
+          sx={{ width: 1 }}
+        />
         </Stack>
+      </Stack> 
 
-
-      </Stack>
       <Divider sx={{ borderStyle: "dashed", mb: 1 }} />
-      <Box sx={{ p: 3 }}>
-        <Button
-          //             fullWidth
-          size="large"
-          type="submit"
-          color="primary"
-          variant="outlined"
-          startIcon={<Iconify icon="ic:round-clear-all" />}
-          onClick={() => {
-            reset();
-            dispatch(handleClearFilters());
-          }}
-        >
-          Clear All
-        </Button>
-      </Box>
     </FormProvider>
   );
 }
