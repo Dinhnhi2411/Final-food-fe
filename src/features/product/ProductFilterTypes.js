039d45de5109718d9f04ff36041cf59eadb3b579
Đinh Ngọc Liên Nhi <dinhngocliennhi1999@gmail.com>
2023-02-25 12:18:46 +0700

    Update ProductFilterTypes.js

--- src/features/product/ProductFilterTypes.js
+++ src/features/product/ProductFilterTypes.js
@@ -1,36 +1,20 @@
 // material
-import FilterAltOutlinedIcon from '@mui/icons-material/FilterAltOutlined';
-
-import {
-  Divider,
-  InputAdornment,
-  Stack,
-  Typography,
-} from "@mui/material";
+import { Divider, Stack, Typography } from "@mui/material";
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
-
 import { useSelector } from "react-redux";
-import { FormProvider, FSelect } from "../../components/form";
+import { FMultiCheckbox, FormProvider } from "../../components/form";
 
-export const FILTER_TYPES_OPTIONS = [
-  { value: "", label: "All" },
-  { value: "Fruit", label: "Fruit" },
-  { value: "Vegetable", label: "Vegetable" },
-
-];
-
+export const FILTER_TYPES_OPTIONS = ["Fruit", "Vegetable"];
 
 const defaultValues = {
-  sortBy: "",
+  types: "",
 };
-
 
 export default function ProductFilterTypes({ handleDispatch }) {
   const { filters } = useSelector((state) => state?.product);
   const methods = useForm({ defaultValues, mode: "onChange" });
   const { handleSubmit, reset, watch } = methods;
-  
 
   useEffect(() => {
     if (!filters.sortBy) {
@@ -46,39 +30,25 @@ export default function ProductFilterTypes({ handleDispatch }) {
 
   return (
     <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
-      <Stack  sx={{ p: 3, width: 200 }}>
-        <Stack spacing={1} sx={{mb:4}}>
+      <Stack sx={{ p: 2, width: 250 }}>
+        <Stack spacing={1} sx={{ mb: 1 }}>
           <Typography
             sx={{
-              color:"primary.main",
+              color: "primary.main",
               fontSize: { xs: 15, md: 17, lg: 18 },
-              
-              textAlign: "center",
+              // textAlign: "center",
             }}
           >
             Types
           </Typography>
-          <FSelect
-            name="sortBy"
-            size="small"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <FilterAltOutlinedIcon/>
-                </InputAdornment>
-              ),
-            }}
-          >
-            {FILTER_TYPES_OPTIONS.map((option) => (
-              <option key={option.value} value={option.value} sx={{ p: 4 }}>
-                {option.label}
-              </option>
-            ))}
-          </FSelect>
+          <FMultiCheckbox
+            name="types"
+            options={FILTER_TYPES_OPTIONS}
+            sx={{ width: 1, color:"secondary" }}
+          />
         </Stack>
       </Stack>
       <Divider sx={{ borderStyle: "dashed" }} />
-
     </FormProvider>
   );
 }
