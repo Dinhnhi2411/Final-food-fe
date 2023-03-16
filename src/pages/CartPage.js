c4537c03ffc15bbc687e555cb76d3b620544e6f0
Đinh Ngọc Liên Nhi <dinhngocliennhi1999@gmail.com>
2023-03-16 15:44:45 +0700

    Update CartPage.js

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,23 +1,67 @@
-import React from "react";
+import { Box, Stack, Step, StepButton, Stepper } from "@mui/material";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import CartList from "../features/cart/CartList";
-import { getCart } from "../features/cart/cartSlice";
+import CartOrder from "../features/cart/CartOrder";
+import { getCart, setActiveStep } from "../features/cart/cartSlice";
 import useAuth from "../hooks/useAuth";
 
 function CartPage() {
   const { user } = useAuth();
   const dispatch = useDispatch();
-  let { carts, page } = useSelector((state) => state?.cart);
- 
 
+  let { carts, page , activeStep} = useSelector((state) => state?.cart);
+
+  const handleStep = (step) => {
+    dispatch(setActiveStep(step));
+  };
   useEffect(() => {
     if (user) dispatch(getCart(page));
-  }, [dispatch, page, user]);
+  }, [dispatch, page, user, activeStep]);
+
+  const handleActiveStep = () => {
+    dispatch(setActiveStep(activeStep + 1));
+  };
+
+  const STEPS = [
+    {
+      value: "Cart",
+      Cart: 0,
+      component: <CartList carts={carts} handleActiveStep={handleActiveStep} />,
+    },
+
+
+    {
+      value: "Payment",
+      Payment: 1,
+      component: <CartOrder carts={carts.Carts} handleActiveStep={handleActiveStep}/>,
+    },
+
+  ];
 
   return (
     <>
-      <CartList carts={carts} />
+      <Stack spacing={3}>
+        <Box sx={{ width: { sx: "100%", md: "60%" }, mx: "auto" }}>
+          <Stepper nonLinear activeStep={activeStep} sx={{ flexWrap: "wrap" }}>
+            {STEPS.map((step, index) => (
+              <Step key={step[step.value]}>
+                <StepButton onClick={() => handleStep(step[step.value])}>
+                  {step.value}
+                </StepButton>
+              </Step>
+            ))}
+          </Stepper>
+        </Box>
+
+        {STEPS.map((step, idx) => {
+          const isMatched = step[step.value] === activeStep;
+          return (
+            isMatched && <Box key={step[step.value]}>{step.component}</Box>
+          );
+        })}
+      </Stack>
     </>
   );
 }
